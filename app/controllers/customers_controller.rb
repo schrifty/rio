84235b60ab416297e22cf3ae1296c492a5cdada8
marcschriftman <schrifty@gmail.com>
2013-08-19 17:28:58 -0500

    Secure CustomersController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,27 +2,39 @@ class CustomersController < ApplicationController
   before_filter :authenticate_agent!
 
   def create
-    begin
-      @customer = Customer.create!(customer_params)
-      return render json: @customer, status: 201
-    rescue ActiveRecord::RecordInvalid => e
-      return render text: e.message, status: 422
+    Customer.transaction do
+      begin
+        @customer = Customer.create!(customer_params)
+        return render json: @customer, status: 201
+      rescue ActiveRecord::RecordInvalid => e
+        render text: e.message, status: 422
+        raise ActiveRecord::Rollback
+      rescue Exception => e
+        render text: e.message, status: 500
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def update
-    begin
-      @customer = Customer.find(params[:id])
-      @customer.update_attributes!(customer_params)
-      return render json: @customer, status: 200
-    rescue ActiveRecord::RecordInvalid => e
-      return render text: e.message, status: 422
+    Customer.transaction do
+      begin
+        @customer = Customer.by_tenant(current_agent.tenant).find(params[:id])
+        @customer.update!(update_params)
+        return render json: @customer, status: 200
+      rescue ActiveRecord::RecordInvalid => e
+        render text: e.message, status: 422
+        raise ActiveRecord::Rollback
+      rescue Exception => e
+        render text: e.message, status: 500
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def show
     begin
-      @customer = Customer.find(params[:id])
+      @customer = Customer.by_tenant(current_agent.tenant).find(params[:id])
       return render json: @customer
     rescue ActiveRecord::RecordNotFound => e
       return render text: e.message, status: 404
@@ -30,7 +42,7 @@ class CustomersController < ApplicationController
   end
 
   def index
-    @customers = Customer.all
+    @customers = Customer.by_tenant(current_agent.tenant).all
     return render json: @customers
   end
 
@@ -39,4 +51,7 @@ class CustomersController < ApplicationController
     params.require(:customer).permit(:tenant_id, :display_name)
   end
 
+  def update_params
+    params.require(:customer).permit(:display_name)
+  end
 end
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -6,84 +6,92 @@ describe CustomersController do
   before {
     controller.stub(:authenticate_agent!).and_return true
     @tenant1 = create(:tenant)
+    @tenant2 = create(:tenant)
+    @tenant3 = create(:tenant)
+
+    @agent1 = create(:agent, :tenant => @tenant1)
+
+    @customer1 = create(:customer, :tenant => @tenant1)
+    @customer2 = create(:customer, :tenant => @tenant2)
+    @customers = [@customer1, @customer2] + (1..10).map { |n|
+      tenant = [@tenant1, @tenant2, @tenant3][rand(3)]
+      create(:customer, :tenant => tenant)
+    }
+
+    sign_in @agent1
   }
 
   describe 'GET :index' do
-    before {
-      @customers = (1..3).map { |n| create(:customer, :tenant => @tenant1) }
-    }
-
-    it 'should return all customers' do
+    it 'should get all accessible customers and only those customers' do
       get :index
       expect(response.status).to eq(200)
-      assigns(:customers).map { |c| c.id }.should eq @customers.map { |c| c.id }
+      assigns(:customers).map { |c| c.id }.should eq @customers.select { |c| c.tenant == @customer1.tenant }.map { |c| c.id }
     end
   end
 
   describe 'GET :show' do
-    let (:customers) { (1..3).map { |n| create(:customer, :tenant => @tenant1) } }
-
     it 'should return one customer' do
-      get :show, :id => customers[0].id
-      expect(response.status).to eq(200)
-      assigns(:customer).should eq(customers[0])
+      get :show, :id => @customer1.id
+      expect(response.status).to eq 200
+      assigns(:customer).should eq @customer1
     end
 
     it 'should return a 404 with a nonexistent customer' do
       get :show, :id => 999
       expect(response.status).to eq(404)
     end
+
+    it 'should not find and return a conversation from another tenant' do
+      get :show, :id => @customer2.id
+      expect(response.status).to eq 404
+    end
   end
 
   describe 'POST :create' do
-    before do
-      @tenant = create(:tenant)
+    it 'should create a customer' do
+      expect {
+        post :create, :customer => {:tenant_id => @tenant1.id, :display_name => 'Customer X'}
+      }.to change(Customer, :count).by(1)
+      expect(response.status).to eq(201)
     end
 
-    context 'with valid attributes' do
-      it 'should create a customer' do
-        expect {
-          post :create, :customer => {:tenant_id => @tenant.id, :display_name => 'Customer X'}
-        }.to change(Customer, :count).by(1)
-        expect(response.status).to eq(201)
-      end
+    it 'should not create a customer with no tenant' do
+      expect {
+        post :create, customer: {:display_name => 'Customer Y'}
+      }.to change(Customer, :count).by(0)
+      expect(response.status).to eq(422)
     end
 
-    context 'with invalid attributes' do
-      it 'should not create a customer' do
-        expect {
-          post :create, customer: {:display_name => 'Customer Z'}
-        }.to change(Customer, :count).by(0)
-        expect(response.status).to eq(422)
-      end
-    end
+    # TBD - We have to protect create so a tenant can't pollute another tenant's spaces
+    #it 'should not create a customer with a forbidden tenant' do
+    #  expect {
+    #    post :create, customer: {:tenant_id => @tenant2.id, :display_name => 'Customer Z'}
+    #  }.to change(Customer, :count).by(0)
+    #  expect(response.status).to eq(422)
+    #end
   end
 
   describe 'PUT :update' do
-    let (:customer) { create(:customer, :tenant => @tenant1) }
-
-    context 'with valid attributes' do
-      it 'should find the customer' do
-        put :update, :customer => {:display_name => 'Customer Y'}, :id => customer.id
-        assigns(:customer).should eq(customer)
-        expect(response.status).to eq(200)
-      end
-
-      it 'should update customer attributes' do
-        put :update, :customer => {:display_name => 'Customer Y'}, :id => customer.id
-        customer.reload
-        customer.display_name.should eq 'Customer Y'
-        expect(response.status).to eq(200)
-      end
+    it 'should find the customer' do
+      put :update, :customer => {:display_name => 'Customer Y'}, :id => @customer1.id
+      assigns(:customer).should eq @customer1
+      expect(response.status).to eq 200
     end
 
-    context 'with invalid attributes' do
-      it 'should not update a customer' do
-        customer = create(:customer)
-        put :update, :customer => {:tenant_id => nil}, :id => customer.id
-        expect(response.status).to eq(422)
-      end
+    it 'should update customer attributes' do
+      put :update, :customer => {:display_name => 'Customer Z'}, :id => @customer1.id
+      @customer1.reload
+      @customer1.display_name.should eq 'Customer Y'
+    end
+
+    it 'should not update a customer with an empty display_name' do
+      put :update, :customer => {:display_name => nil}, :id => @customer1.id
+      expect(response.status).to eq 422
+    end
+
+    it 'should not update the tenant of a customer' do
+      put :update, :customer => {:tenant_id => @tenant2}, :id => @customer1.id
+      expect(@customer1.tenant_id).to eq @tenant1.id
     end
   end
-
 end
