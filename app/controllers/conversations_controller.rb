de80a196c49d5d664d586eb6e1b651e785d5fe8e
marcschriftman <schrifty@gmail.com>
2013-08-19 16:28:56 -0500

    Secure ConversationsController

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,28 +2,44 @@ class ConversationsController < ApplicationController
   before_filter :authenticate_agent!
 
   def create
-    begin
-      @conversations = [ Conversation.create!(conversation_params) ]
-      return render json: @conversations, status: 201
-    rescue ActiveRecord::RecordInvalid => e
-      return render text: e.message, status: 422
+    Conversation.transaction do
+      begin
+        @conversations = [Conversation.create!(conversation_params)]
+        return render json: @conversations, status: 201
+      rescue ActiveRecord::RecordInvalid => e
+        return render text: e.message, status: 422
+        raise ActiveRecord::Rollback
+      rescue Exception => e
+        render text: e.message, status: 500
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def update
-    begin
-      @conversation = Conversation.find(params[:id])
-      @conversation.update_attributes!(conversation_params)
-      return render json: @conversation, status: 200
-    rescue ActiveRecord::RecordInvalid => e
-      return render text: e.message, status: 422
+    Conversation.transaction do
+      begin
+        @conversation = Conversation.by_tenant(current_agent.tenant).find(params[:id])
+        @conversation.update!(conversation_params)
+        return render json: @conversation, status: 200
+      rescue ActiveRecord::RecordInvalid => e
+        render text: e.message, status: 422
+        raise ActiveRecord::Rollback
+      rescue Exception => e
+        render text: e.message, status: 403
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def show
     begin
-      @conversation = Conversation.find(params[:id])
-      return render json: @conversation
+      @conversation = Conversation.by_tenant(current_agent.tenant).find(params[:id])
+      if request.head?
+        return render status: 200
+      else
+        return render json: @conversation
+      end
     rescue ActiveRecord::RecordNotFound => e
       return render text: e.message, status: 404
     end
@@ -33,11 +49,11 @@ class ConversationsController < ApplicationController
   end
 
   def index
-    @conversations = Conversation.all
+    @conversations = Conversation.by_tenant(current_agent.tenant)
     return render json: @conversations
   end
 
-private
+  private
   def conversation_params
     params.require(:conversation).permit(:tenant_id, :active, :customer_id, :referer_url, :location, :customer_data, :first_customer_message, :engaged_agent_id, :preferred_response_channel, :preferred_response_channel_info)
   end
--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -6,88 +6,124 @@ describe ConversationsController do
   before {
     controller.stub(:authenticate_agent!).and_return true
     @tenant1 = create(:tenant)
-    @customer1 = create(:customer)
+    @tenant2 = create(:tenant)
+    @tenant3 = create(:tenant)
+
+    @agent1 = create(:agent, :tenant => @tenant1)
+
+    @customer1 = create(:customer, :tenant => @tenant1)
+    @customer2 = create(:customer, :tenant => @tenant2)
+
+    @conversation1 = create(:conversation, :tenant => @tenant1, :customer => @customer1)
+    @conversation2 = create(:conversation, :tenant => @tenant2, :customer => @customer2)
+    @conversations = [@conversation1, @conversation2] + (1..10).map { |n|
+      tenant = [@tenant1, @tenant2, @tenant3][rand(3)]
+      customer = create(:customer, :tenant => tenant)
+      create(:conversation, :tenant => tenant, :customer => customer)
+    }
   }
 
   describe 'GET :index' do
     before {
-      @conversations = (1..3).map { |n| create(:conversation, :tenant => @tenant1, :customer => @customer1) }
+      sign_in @agent1
     }
 
-    it 'should get all conversations' do
+    it 'should get all and only all of the conversations the user is permitted to see' do
       get :index
       expect(response.status).to eq(200)
-      assigns(:conversations).map { |c| c.id }.should eq @conversations.map { |c| c.id }
+      assigns(:conversations).map { |a| a.id }.should eq @conversations.select { |a| a.tenant == @conversation1.tenant }.map { |a| a.id }
     end
   end
 
   describe 'GET :show' do
-    let (:conversations) { (1..3).map { |n| create(:conversation, :tenant => @tenant1, :customer => @customer1) } }
+    before {
+      sign_in @agent1
+    }
 
     it 'should find and return a conversation' do
-      get :show, :id => conversations[0].id
-      expect(response.status).to eq(200)
-      assigns(:conversation).should eq(conversations[0])
+      get :show, :id => @conversation1.id
+      expect(response.status).to eq 200
+      assigns(:conversation).should eq @conversation1
+    end
+
+    it 'should not find and return a conversation from another tenant' do
+      get :show, :id => @conversation2.id
+      expect(response.status).to eq 404
     end
 
     it 'should return a 404 when looking for a nonexistent conversation' do
       get :show, :id => 999
-      expect(response.status).to eq(404)
+      expect(response.status).to eq 404
     end
   end
 
   describe 'POST create' do
-    before do
-      @tenant = create(:tenant)
-      @customer = create(:customer)
+    before {
+      sign_in @agent1
+    }
+
+    it 'should create a valid conversation' do
+      expect {
+        post :create, :conversation => {:tenant_id => @tenant1.id, :customer_id => @customer1.id, :active => true, :referer_url => 'http:'}
+      }.to change(Conversation, :count).by(1)
+      expect(response.status).to eq(201)
     end
 
-    context 'with valid attributes' do
-      it 'should create a conversation' do
-        expect {
-          post :create, :conversation => {:tenant_id => @tenant.id, :customer_id => @customer.id, :active => true, :referer_url => 'http:'}
-        }.to change(Conversation, :count).by(1)
-        expect(response.status).to eq(201)
-      end
+    it 'should not create a conversation without a tenant' do
+      expect {
+        post :create, conversation: {:customer_id => @customer1.id, :active => true, :referer_url => 'http:'}
+      }.to change(Conversation, :count).by(0)
+      expect(response.status).to eq(422)
     end
 
-    context 'with invalid attributes' do
-      it 'should not create a conversation' do
-        expect {
-          post :create, conversation: {:customer_id => @customer.id, :active => true, :referer_url => 'http:'}
-        }.to change(Conversation, :count).by(0)
-        expect(response.status).to eq(422)
-      end
+    it 'should not create a conversation with a forbidden tenant' do
+      expect {
+        post :create, conversation: {:tenant_id => @tenant2.id, :customer_id => @customer1.id, :active => true, :referer_url => 'http:'}
+      }.to change(Conversation, :count).by(0)
+      expect(response.status).to eq(422)
+    end
+
+    it 'should not create a conversation with a forbidden customer' do
+      expect {
+        post :create, conversation: {:tenant_id => @tenant1.id, :customer_id => @customer2.id, :active => true, :referer_url => 'http:'}
+      }.to change(Conversation, :count).by(0)
+      expect(response.status).to eq(422)
     end
   end
 
   describe 'PUT :update' do
-    let (:conversation) { create(:conversation) }
-    let (:agent) { create(:agent, :tenant => @tenant1) }
+    before {
+      sign_in @agent1
+    }
 
-    context 'with valid attributes' do
-      it 'should find the conversation' do
-        put :update, :conversation => {:active => false}, :id => conversation.id
-        assigns(:conversation).should eq(conversation)
-        expect(response.status).to eq(200)
-      end
-
-      it 'should update agent attributes' do
-        put :update, :conversation => {:active => false, :referer_url => 'www.wow.com', :engaged_agent_id => agent.id}, :id => conversation.id
-        conversation.reload
-        conversation.active.should eq false
-        conversation.referer_url.should eq 'www.wow.com'
-        conversation.engaged_agent.id.should eq agent.id
-        expect(response.status).to eq(200)
-      end
+    it 'should find the conversation' do
+      put :update, :conversation => {:active => false}, :id => @conversation1.id
+      assigns(:conversation).should eq @conversation1
+      expect(response.status).to eq 200
     end
 
-    context 'with invalid attributes' do
-      it 'should not update a conversation' do
-        conversation = create(:conversation)
-        put :update, :conversation => {:customer_id => nil}, :id => conversation.id
-        expect(response.status).to eq(422)
-      end
+    it 'should update agent attributes' do
+      put :update, :conversation => {:active => false, :referer_url => 'www.wow.com', :engaged_agent_id => @agent1.id}, :id => @conversation1.id
+      @conversation1.reload
+      @conversation1.active.should eq false
+      @conversation1.referer_url.should eq 'www.wow.com'
+      @conversation1.engaged_agent.id.should eq @agent1.id
+      expect(response.status).to eq 200
+    end
+
+    it 'should not find a forbidden conversation' do
+      put :update, :conversation => {:active => false}, :id => @conversation2.id
+      expect(response.status).to eq 403
+    end
+
+    it 'should not update a conversation without a customer' do
+      put :update, :conversation => {:customer_id => nil}, :id => @conversation1.id
+      expect(response.status).to eq 422
+    end
+
+    it 'should not update a conversation with a forbidden customer' do
+      put :update, :conversation => {:customer_id => @customer2.id}, :id => @conversation1.id
+      expect(response.status).to eq 422
     end
   end
 end
