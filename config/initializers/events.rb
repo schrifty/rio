2592c64515b77bbb8d4373539dba9241120cd149
marcschriftman <schrifty@gmail.com>
2013-09-05 15:50:30 -0500

    Fix prod mysql creds

--- config/initializers/events.rb
+++ config/initializers/events.rb
@@ -26,7 +26,6 @@ WebsocketRails.setup do |config|
   # Uncomment and edit to point to a different redis instance.
   # Will not be used unless standalone or synchronization mode
   # is enabled.
-  Rails.logger.info { "XYZZY: #{ws_config[Rails.env]['redis-host']}" }
   config.redis_options = {:host => ws_config[Rails.env]['redis-host'], :port => ws_config[Rails.env]['redis-port']}
 
   # By default, all subscribers in to a channel will be removed
